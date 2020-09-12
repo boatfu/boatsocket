26d06b146f51c85e9a7e7e9bbb95c0cff7a71090
boatfu <boaterf@gmail.com>
2020-09-12 01:17:28 -0400

    chore: add tls support on websocket

--- src/client.js
+++ src/client.js
@@ -2,25 +2,22 @@ const http = require('http');
 const url = require('url');
 const crypto = require('crypto');
 const Websocket = require('ws');
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 function handleError(e) {
     console.error(e);
 }
 function connect(request, cSock) {
-    console.log('hello connect');
     const u = url.parse('http://' + request.url);
-    const ws = new Websocket('ws://localhost:9999');
+    const ws = new Websocket('wss://localhost:9999');
     ws.on('open', () => {
-        console.log('open!')
         cSock.write('HTTP/1.1 200 Connection Established\r\n\r\n');
-        ws.send(u.port + '-' + u.hostname);
+        ws.send(u.port + '-' + u.hostname + '-' + 'password');
     });
     ws.on('message', (message) => {
-        console.log('will send back data', message);
         cSock.write(message);
     });
     ws.on('error', handleError);
     cSock.on('data', (data) => {
-        console.log('will send data', data);
         ws.send(data);
     });
     cSock.on('error', handleError);
--- src/server.js
+++ src/server.js
@@ -1,32 +1,38 @@
+const https = require('https');
 const http = require('http');
 const net = require('net');
-const url = require('url');
 const crypto = require("crypto");
+const fs = require('fs');
+const selfSigned = require('openssl-self-signed-certificate');
 
 function handleError(e) {
     console.error(e);
 }
+const options = {
+    key: selfSigned.key,
+    cert: selfSigned.cert
+}
+const httpsServer = https.createServer(options).listen(9999, '0.0.0.0');
 const WebsocketServer = require('ws').Server;
 const wss = new WebsocketServer({
-    port: 9999
+    server: httpsServer
 });
 wss.on('connection', (ws) => {
     let count = 1;
     let sSock = null;
     ws.on('message', (message) => {
         if (count === 1) {
-            count = 0;
             let arr = message.split('-');
-            sSock = net.Socket().connect(arr[0], arr[1], () => {
-                console.log('connected');
-            });
+            count = 0;
+            if (arr[2] !== 'password') {
+                ws.close();
+            }
+            sSock = net.Socket().connect(arr[0], arr[1]);
             sSock.on('data', (data) => {
-                console.log('receive data', data);
                 ws.send(data);
             });
             sSock.on('error', handleError);
         } else {
-            console.log('send message', message);
             sSock.write(message);
         }
     });
