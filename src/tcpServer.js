c306038536c33155ec653a10d19b0ebe0f92d139
boatfu <boaterf@gmail.com>
2020-09-14 07:15:46 -0400

    chore: replace count with once

--- src/tcpServer.js
+++ src/tcpServer.js
@@ -13,33 +13,37 @@ const options = {
     cert: selfSigned.cert
 }
 
-const server = tls.createServer(options, (cSock) => {
-    let count = 1 ;
+function handShake(data) {
+    const cSock = this;
     let sSock = null;
-    cSock.on('data', (data) => {
-        if (count === 1) {
-            count = 0;
-            const arr = data.toString().split('&');
-            sSock = new net.Socket().connect(arr[0], arr[1], () => {
-                sSock.on('data', (data) => {
-                    cSock.write(data);
-                });
-            });
-            sSock.on('error', handleError('sSock error', () => {
-                sSock.destroy();
-            }));
-            sSock.on('close', () => {
-                sSock.destroy();
-            })
-        } else {
-            sSock.write(data);
-        }
+    const arr = data.toString().split('&');
+    sSock = new net.Socket().connect(arr[0], arr[1], () => {
+        sSock.on('data', (data) => {
+            cSock.write(data);
+        });
     });
+    cSock.on('data', handleProxy.bind(sSock));
+    sSock.on('error', handleError('sSock error', () => {
+        sSock.destroy();
+    }));
+    sSock.on('close', () => {
+        sSock.destroy();
+    });
+}
+
+function handleProxy(data) {
+    const sSock = this;
+    sSock.write(data);
+}
+
+const server = tls.createServer(options, (cSock) => {
+    cSock.once('data', handShake.bind(cSock));
     cSock.on('error', handleError('cSock error', () => {
         cSock.destroy();
-    }))
+    }));
     cSock.on('close', () => {
         cSock.destroy();
     });
 });
+
 server.listen(9999, '0.0.0.0');
